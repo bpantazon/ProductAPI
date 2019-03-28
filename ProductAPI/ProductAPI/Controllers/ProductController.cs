4de6fbcd947a44cf9fb1f83cc77ce00362861a35
Ben Pantazon <pantazon-b@live.com>
2019-03-28 14:41:34 -0500

    First commit

---
+++ ProductAPI/ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using ProductAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        CompanyProductsDataContext db = new CompanyProductsDataContext();
+        // GET: api/Product
+        public IEnumerable<CompanyProduct> Get()
+        {
+            var allProducts = db.CompanyProducts.ToList();
+
+            return allProducts;
+        }
+
+        // GET: api/Product/5
+        [HttpGet]
+        public CompanyProduct GetProductDetails(int id)
+        {
+            var product = db.CompanyProducts.Where(p => p.Product_Id == id).SingleOrDefault();
+            return product;
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public void Post(CompanyProduct product)
+        {       
+                db.CompanyProducts.InsertOnSubmit(product);
+
+                db.SubmitChanges();         
+        }
+
+        // PUT: api/Product/5
+        [HttpPut]
+        public void Put(int id, [FromBody]string value)
+        {
+
+        }
+
+        // DELETE: api/Product/5
+        [HttpDelete]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ProductAPI/ProductAPI/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string BrandName { get; set; }
+        public string ModelName { get; set; }
+        public decimal Price { get; set; }
+        public string SKU { get; set; }
+        public string Category { get; set; }
+        public int Count { get; set; }
+    }
+}
